b1a906c61a07bcf96c941e9538c933a3c67e35cb
jingyiz86 <156809909+jingyiz86@users.noreply.github.com>
2024-03-23 00:49:07 +0800

    Update run.sh

--- NTIRE2024_ESR-main/run.sh
+++ NTIRE2024_ESR-main/run.sh
@@ -2,7 +2,7 @@
 CUDA_VISIBLE_DEVICES=0 python test_demo.py \
     --data_dir [path to your data dir] \
     --save_dir [path to your save dir] \
-    --model_id 0
+    --model_id 25
 
 # When Test datasets are included 
 # CUDA_VISIBLE_DEVICES=0 python test_demo.py \
